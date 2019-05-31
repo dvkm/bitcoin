62e9172b7d62f66b72b30bc66b44ce8ba1c78aa3
David Kim <dk432@njit.edu>
2019-05-30 21:37:13 -0400

    Shrimp Tempura binary files

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -13,7 +13,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"org.bitcoinfoundation.Bitcoin-Qt";
+        return @"kim.vid.ShrimpTempura-Qt";
     } else {
         return [self __bundleIdentifier];
     }
